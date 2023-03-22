58be1a3298b0ee46f12e28f893f563a69d4b2147
mwang840 <maxwang@udel.edu>
2023-03-22 15:28:08 -0400

    feat register input for the file itself

--- src/moves/registerInput.ts
+++ src/moves/registerInput.ts
@@ -1,13 +1,23 @@
-import Phaser, {Physics} from "phaser";
-
+import Phaser from "phaser";
+import Player from "../Classes/player";
 export default class registerInput extends Phaser.Scene{
-     
+
     constructor(){
         super("Input Registered")
-        const keys: string[] = ["walk_forward", "walk_back", "jump_forward", "kick", "punch", "uppercut", "crhook", "roundhouse"];
+        const avaliable_keys: string[] = ["walk", "walk_back", "jump_forward", "kick", "punch", "uppercut", "crhook", "roundhouse"];
+        const p1Keys: string[] = ["walk", "jump_forward", "punch", "crhook"]
+        const p2Keys: string[] = ["walk_back","kick","uppercut","roundhouse"]
+        const current_key = avaliable_keys[0];
     }
 
-    private registerInput(input: string[]){
+
+    //Calls the players registers input first
+    public registerCurrentInput(input: string, spaces:number, player1: Player, player2: Player){
+
+        console.log("Punch called");
+        
+        player1?.playerAttack(spaces, input)
+        player2?.playerAttack(spaces, input)
         return
     }
 
