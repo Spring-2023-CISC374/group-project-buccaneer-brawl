dc5c533e93fb2caa984373defaebb95ef9d6913f
mwang840 <maxwang@udel.edu>
2023-04-05 16:10:56 -0400

    note need to know why getTime is throwing an undefined reference

--- src/moves/registerInput.ts
+++ src/moves/registerInput.ts
@@ -14,23 +14,29 @@ export default class registerInput extends Phaser.Scene{
     public validInput(input: string[], spaces: number, player1?: Player, player2?: Player){
         //Check if the input is considered a non-fighting move for loop, reset to start!!!
         let index = 0;
-        for(index = 0; index < input.length; index++){
-            if(this.keys_to_choose[index] === "walk" || this.keys_to_choose[index] === "walk_back" || this.keys_to_choose[index] === "jump_forward"){
-                player1?.movePlayer(spaces, input[index]);
-                player2?.movePlayer(spaces, input[index]);
-            }
-            else{
-                player1?.playerAttack(spaces, input[index]);
-                player2?.playerAttack(spaces, input[index]);
-            }
-            if(index === input.length){
-                index = 0;
+        let timer = 0;
+
+        while(index < input.length) {
+            if(timer <= this.game.getTime()) {
+            
+                if(this.keys_to_choose[index] === "walk" || this.keys_to_choose[index] === "walk_back" || this.keys_to_choose[index] === "jump_forward"){
+                    player1?.movePlayer(spaces, input[index]);
+                    player2?.movePlayer(spaces, input[index]);
+                }
+                else{
+                    player1?.playerAttack(spaces, input[index]);
+                    player2?.playerAttack(spaces, input[index]);
+                }
+                if(index === input.length){
+                    index = 0;
+                }
+                timer += this.game.getTime() + 1000;
+                index++;
             }
         }
-        //Otherwise check if the input is a fighting input!!
-        
-        return
+
     }
+
     
 
 }
