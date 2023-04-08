8f0965aa8d3c4e87a3f638628db4ebec75146e64
Christopher Bennett <cbcolleg@udel.edu>
2023-04-08 09:16:14 -0400

    merged robs changes, fixed changescene

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import Phaser from 'phaser'
 
-import FightScene from './FightScene'
+import FightScene from './FightScene';
+import InputScene from './moves/InputScene';
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 1200 },
-		},
-	},
-	scene: [FightScene],
-}
+  type: Phaser.AUTO,
+  parent: 'app',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 1200 },
+    },
+  },
+  scene: [InputScene, FightScene],
+};
 
 export default new Phaser.Game(config)
