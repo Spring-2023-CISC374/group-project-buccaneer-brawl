4139d7eb67d9e1d3da2def707d6593c92c2cabf0
mwang840 <maxwang@udel.edu>
2023-04-17 14:22:44 -0400

    fixed instructions scene

--- src/Scenes/InstructionScene.ts
+++ src/Scenes/InstructionScene.ts
@@ -16,13 +16,12 @@ export default class InstructionScene extends Phaser.Scene{
        titlescreen.scaleX = 2.5;
        titlescreen.scaleY = 1.5
        //Adds instructions title
-       const instructionsTitle = this.add.text(width / 2, height / 2, 'Instructions', {
+       this.add.text(width / 4, height / 4, 'Instructions', {
         fontSize: '64px',
         fontFamily: 'Arial',
         color: '#ffffff',
         backgroundColor: '#000000',
       });
-      instructionsTitle.setOrigin(0.675, 2);
       //Includes information from the instructions
       const instructionsInfo = this.add.text(width / 2, height / 2, 'Ahoy, today in the game the pirates need your input.', {
         fontSize: '16px',
@@ -81,7 +80,7 @@ export default class InstructionScene extends Phaser.Scene{
       });
       backToStartButton.setInteractive({ useHandCursor: true });
       backToStartButton.on("pointerdown", ()=>{
-      this.scene.start("InputScene");
+      this.scene.start("TitleScene");
     })
     }
 }
