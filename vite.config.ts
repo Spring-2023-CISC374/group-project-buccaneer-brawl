78a6eb068587e1d7ddf874534d76bb6346121b32
Christopher Bennett <cbcolleg@udel.edu>
2023-04-16 22:29:32 -0400

    comitt 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 export default defineConfig({
-	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
-	clearScreen: false,
-	base: "/gh-pages/"
-})
+  plugins: [chunkSplitPlugin()],
+  server: { host: "0.0.0.0", port: 8000 },
+  clearScreen: false,
+  root: ".",
+  base: "./",
+});
