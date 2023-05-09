6b7f3a0361a832d1b8d725000db79c10c6734d42
Tavong73 <tavong73@gmail.com>
2023-05-09 17:38:08 -0400

    Trying it out

---
+++ src/Scenes/Photo1Scene.ts
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+
+export default class Photo1Scene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'Photo1Scene' });
+      }
+      preload() {
+        //Photo 1
+        this.load.image('Photo 1', 'assets/InstructionPhotos/Photo 1.jpg');
+      }
+      create() {
+      const { width, height } = this.scale;
+      // Add Next Button
+      const NextButton = this.add.text(width / 2, height / 2, 'Next', {
+        fontSize: '48px',
+        fontFamily: 'Arial',
+        color: '#ffffff',
+        backgroundColor: '#000000',
+        padding: {
+          x: 16,
+          y: 8,
+        },
+      });
+  
+  
+  
+      NextButton.setOrigin(0.5);
+      NextButton.setInteractive({ useHandCursor: true });
+      NextButton.on('pointerdown', () => {
+        this.scene.start('InputScene');
+      });
+    }
+}
